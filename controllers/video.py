c0d772e7750cb0d29a091b3c3db610eef52e2c49
Sergio <Sergio@amf.com>
2020-06-12 00:37:19 +0100

    rewiew_update

--- controllers/video.py
+++ controllers/video.py
@@ -6,22 +6,18 @@ def index(): return dict(message=auth.user_id)
 def review():
     # Devolve vidios   com estado Visivel       ordenado pela data decrescente
     videos = db(Videos.estado == 'Visivel').select(orderby=~Videos.dtCriacao)
-    
-    if auth.user_id != None:
-        #Devolve lista das subscrições
-        user = db(db.auth_user.id == auth.user_id).select().first()
-        subscricao = user.subscricao
-        
-        query = 'SELECT * FROM categoria WHERE categoria.id IN('
 
-        for cat in subscricao:
-            query = query + str(cat) +', '
+    #Devolve lista das subscrições
+    user = db(db.auth_user.id == auth.user_id).select().first()
+    subscricao = user.subscricao
 
-        query = query[0:len(query)-2] + ')'
-        categorias = db.executesql(query, as_dict=True)
-    
-        categorias.insert(0, {'descritivo': 'Mais recentes', 'id': '0', 'titulo': 'Mais recentes'})
-    else:
-        categorias = {'descritivo': 'Mais recentes', 'id': '0', 'titulo': 'Mais recentes'}
+    query = 'SELECT * FROM categoria WHERE categoria.id IN('
+
+    for cat in subscricao:
+        query = query + str(cat) +', '
+
+    query = query[0:len(query)-2] + ')'
+    categorias = db.executesql(query, as_dict=True)
+
 
     return dict(categorias=categorias, videos=videos)
